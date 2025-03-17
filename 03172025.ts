19192d258099a2170e0f5fc12c79fde5eecf4eaf
Robin H <robin.hrzg@gmail.com>
2025-03-17 18:19:58 -0500

    Create a stack class.

---
+++ 03172025.ts
@@ -0,0 +1,32 @@
+// Create a stack class
+
+class MyStack {
+  private stack: number[];
+
+  constructor() {
+    this.stack = [];
+  }
+
+  push(num: number): void {
+    this.stack.push(num);
+  }
+
+  pop(): number {
+    return this.stack.pop()!;
+  }
+
+  top(): number {
+    return this.stack.at(-1)!;
+  }
+
+  empty(): boolean {
+    return this.stack.length === 0;
+  }
+}
+
+// num: number. 1 <= num <= 9.
+// At most 100 calls will be made.
+// All the calls to pop() and top() are valid.
+
+// Create a stack class with push(), pop(), top(), and empty() methods.
+// https://leetcode.com/problems/implement-stack-using-queues/
