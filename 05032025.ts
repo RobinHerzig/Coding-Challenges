1cc77f0c5ab56cc4a2fd4db41bce03b5a31a923b
Robin H <robin.hrzg@gmail.com>
2025-05-03 11:27:56 -0500

    Count subarrays where max element appears at least K times. (Sliding window)

---
+++ 05032025.ts
@@ -0,0 +1,41 @@
+// Count subarrays where max element appears at least K times. (Sliding window)
+
+function countSubarrays(nums: number[], k: number): number {
+  let maxElem = Math.max(...nums);
+  let maxElemCount = 0;
+  let subarrayCount = 0;
+  let left = 0;
+  let right = 0;
+
+  while (right < nums.length) {
+    // Step 1: Cache right pointer value.
+    if (nums[right] === maxElem) {
+      maxElemCount++;
+    }
+
+    // Step 2: Verify subarray condition.
+    while (maxElemCount >= k) {
+      subarrayCount += nums.length - right;
+
+      // Step 3: Increment left pointer value.
+      if (nums[left] === maxElem) {
+        maxElemCount--;
+      }
+
+      left++;
+    }
+
+    // Step 4: Increment right pointer value.
+    right++;
+  }
+
+  return subarrayCount;
+}
+
+console.log(countSubarrays([1, 3, 2, 3, 3], 2), 6); // The max value, 3, appears at least twice in 6 subarrays.
+console.log(countSubarrays([1, 4, 2, 1], 3), 0);
+// https://leetcode.com/problems/count-subarrays-where-max-element-appears-at-least-k-times/
+
+// nums: number[]. 1 <= nums.length <= 10^5. 1 <= nums[i] <= 10^6.
+// k: number. 1 <= k <= 10^5.
+// Return the number of subarrays where the maximum element of nums appears at least k time.
