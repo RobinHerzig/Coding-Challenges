2790bb36c9d96c2609b4c179f267ec2e3358bf32
Robin H <robin.hrzg@gmail.com>
2025-05-22 13:44:51 -0500

    Majority Element. (Boyer-Moore majority vote algorithm)

---
+++ 05222025.ts
@@ -0,0 +1,24 @@
+// Majority Element. (Boyer-Moore majority vote algorithm)
+
+function majorityElement(nums: number[]): number {
+  let curr = 0;
+  let vote = 0;
+
+  for (let num of nums) {
+    if (vote === 0) {
+      curr = num;
+    }
+
+    curr === num ? vote++ : vote--;
+  }
+
+  return curr;
+}
+
+console.log(majorityElement([3, 2, 3]), 3);
+console.log(majorityElement([2, 2, 1, 1, 1, 2, 2]), 2);
+console.log(majorityElement([6, 5, 5]), 5);
+// https://leetcode.com/problems/majority-element/
+
+// nums: number[]. 1 <= nums.length <= 5*10^4. -10^9 <= nums[i] <= 10^9.
+// Return the majority element (element appears more than (nums.length / 2) times). Solve in O(n) time and O(1) space.
