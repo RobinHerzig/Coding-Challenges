1e2e2b80b667df454eb86dbad53d718964a7ba10
Robin H <robin.hrzg@gmail.com>
2025-06-21 00:11:52 -0500

    Longest substring without repeating characters. (Sliding window)

---
+++ 06212025.ts
@@ -0,0 +1,38 @@
+// Longest substring without repeating characters. (Sliding window)
+
+function lengthOfLongestSubstring(str: string): number {
+  const map = new Map<string, number>();
+  let length = 0;
+  let left = 0;
+  let right = 0;
+
+  while (right < str.length) {
+    // Step 1: Cash right pointer value.
+    map.set(str[right], (map.get(str[right]) || 0) + 1);
+
+    // Step 2: Validate window condition.
+    if (map.get(str[right]) === 1) {
+      length = Math.max(length, right - left + 1);
+    }
+
+    // Step 3: Increment left pointer.
+    while (map.get(str[right]) > 1) {
+      map.set(str[left], (map.get(str[left]) || 1) - 1);
+      left++;
+    }
+
+    // Step 4: Increment right pointer.
+    right++;
+  }
+
+  return length;
+}
+
+console.log(lengthOfLongestSubstring("abcabcbb"), 3);
+console.log(lengthOfLongestSubstring("bbbbb"), 1);
+console.log(lengthOfLongestSubstring("pwwkew"), 3);
+console.log(lengthOfLongestSubstring(""), 0);
+// https://leetcode.com/problems/longest-substring-without-repeating-characters/
+
+// str: string. 0 <= str.length <= 5*10^4. English letters, digits, symbols, and spaces.
+// Return the length of the longest substring without duplicate characters.
