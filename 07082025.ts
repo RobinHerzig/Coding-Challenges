c247c5345c12a40e103b5d4bdeaafbf9fcbb99a3
Robin H <robin.hrzg@gmail.com>
2025-07-08 12:44:32 -0500

    Find Kth largest element from an array without sorting. (Priority queue)

---
+++ 07082025.ts
@@ -0,0 +1,25 @@
+// Find Kth largest element from an array without sorting. (Priority queue)
+
+function findKthLargest(nums: number[], k: number): number {
+  // Step 1: Create min heap.
+  const minPQ = new MinPriorityQueue();
+
+  // Step 2: Iterate nums. Populate min heap, containing it's size to less than k.
+  for (const num of nums) {
+    minPQ.enqueue(num);
+
+    if (Number(minPQ.size()) > k) {
+      minPQ.dequeue();
+    }
+  }
+
+  return Number(minPQ.dequeue());
+}
+
+console.log(findKthLargest([3, 2, 1, 5, 6, 4], 2), 5); // [6, 5, 4, 3, 2, 1]
+console.log(findKthLargest([3, 2, 3, 1, 2, 4, 5, 5, 6], 4), 4); // [6, 5, 5, 4, 3, 3, 2, 2, 1]
+// https://leetcode.com/problems/kth-largest-element-in-an-array/
+
+// nums: number[]. 1 <= nums.length <= 10^5. -10^4 <= nums[i] <= 10^4.
+// k: number. 1 <= k <= 10^5.
+// Without sorting the array, return the kth largest element from the array.
