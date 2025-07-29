9929fe3dcfed7ec3fe84b2507420d8a90ec2e4cd
Robin H <robin.hrzg@gmail.com>
2025-07-29 14:20:19 -0500

    Longest common subsequence. (Dynamic programming)

---
+++ 07292025.ts
@@ -0,0 +1,33 @@
+// Longest common subsequence. (Dynamic programming)
+
+function longestCommonSubsequence(str1: string, str2: string): number {
+  // Step 1: Declare matrix.
+  const dp = Array.from({ length: str1.length + 1 }, () => new Array(str2.length + 1).fill(0));
+
+  // Step 2: Iterate each character of str1 and str2.
+  for (let i = 1; i < str1.length + 1; i++) {
+    for (let j = 1; j < str2.length + 1; j++) {
+      // Step 3: If characters match, copy diagonal value + 1.
+      if (str1[i - 1] === str2[j - 1]) {
+        dp[i][j] = dp[i - 1][j - 1] + 1;
+        continue;
+      }
+
+      // Step 4: Else, copy larger adjacent value.
+      dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+    }
+  }
+
+  return dp[str1.length][str2.length];
+}
+
+console.log(longestCommonSubsequence("abcde", "ace"), 3);
+console.log(longestCommonSubsequence("abc", "abc"), 3);
+console.log(longestCommonSubsequence("abc", "def"), 0);
+// https://leetcode.com/problems/longest-common-subsequence/
+
+// str1: string. 1 <= str1.length <= 1000. Lowercase English characters.
+// str2: string. 1 <= str2.length <= 1000. Lowercase English characters.
+// Return the length of the longest common subsequence.
+// - A subsequence is a new string with some/no characters deleted.
+// - The relative order of the remaining characters remains the same.
