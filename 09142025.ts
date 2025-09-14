f2566d46b8cbeb83c5c09315c3606ae2c39ceb67
Robin H <robin.hrzg@gmail.com>
2025-09-14 10:35:15 -0700

    Maximum depth of binary tree. (Binary tree) (Breadth-first search)

---
+++ 09142025.ts
@@ -0,0 +1,10 @@
+// Maximum depth of binary tree. (Binary tree) (Breadth-first search)
+
+function maxDepth(root: TreeNode | null): number {
+  
+}
+
+// https://leetcode.com/problems/maximum-depth-of-binary-tree/
+
+// root: TreeNode | null. 0 <= number of nodes <= 10^4. -100 <= Node.val <= 100.
+// Return the tree's maximum depth using a breadth-first search algorithm.
