02e1ea7e429458f332e0a2463e6277bce4602753
Robin H <robin.hrzg@gmail.com>
2025-09-15 14:02:28 -0700

    Binary tree from right side. (Binary tree)

---
+++ 09152025.ts
@@ -0,0 +1,36 @@
+// Binary tree from right side. (Binary tree)
+
+function rightSideView(root: TreeNode | null): number[] {
+  if (!root) {
+    return [];
+  }
+
+  const queue = [root];
+  const result: number[] = [];
+
+  // BFS algorithm
+  while (queue.length) {
+    const length = queue.length;
+    result.push(queue.at(-1).val);
+
+    for (let i = 0; i < length; i++) {
+      const node = queue.shift();
+
+      if (node.left) {
+        queue.push(node.left);
+      }
+
+      if (node.right) {
+        queue.push(node.right);
+      }
+    }
+  }
+
+  return result;
+}
+
+// https://leetcode.com/problems/binary-tree-right-side-view/
+
+// root: TreeNode | null. 0 <= number of nodes <= 10^4. -100 <= Node.val <= 100.
+// Return an array of values that are on the rightmost side for each level.
+// - Arrange from top to bottom.
