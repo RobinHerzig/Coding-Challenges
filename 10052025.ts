8d4ac680339e3382b9cb09af1b1c336a040d9e60
Robin H <robin.hrzg@gmail.com>
2025-10-05 12:06:33 -0500

    Reorder list. (Linked list)

---
+++ 10052025.ts
@@ -0,0 +1,55 @@
+// Reorder list. (Linked list)
+
+function reorderList(head: ListNode | null): void {
+    if (!head.next) {
+        return undefined;
+    }
+
+    // Step 1: Split dataset in half.
+    let fast = head.next;
+    let slow = head;
+
+    while (fast && fast.next) {
+        fast = fast.next.next;
+        slow = slow.next;
+    }
+
+    let a = head;
+    let b = slow.next;
+    slow.next = null;
+
+    // Step 2: Reverse order of second half.
+    let curr = b;
+    let prev = null;
+
+    while (curr) {
+        const next = curr.next;
+
+        curr.next = prev;
+        prev = curr;
+        curr = next;
+    }
+
+    b = prev; // Move b pointer to the now front.
+
+    // Step 3: Combine nodes into single list.
+    while (b) {
+        const nextA = a.next;
+        const nextB = b.next;
+
+        a.next = b;
+        b.next = nextA;
+
+        a = nextA;
+        b = nextB;
+    }
+
+    return undefined;
+}
+
+// [1, 2, 3, 4, 5, 6, 7, 8, 9, 10] => [1, 10, 2, 9, 3, 8, 4, 7, 5, 6]
+// https://leetcode.com/problems/reorder-list/
+
+// head: ListNode | null. 1 <= number of nodes 5*10^4. 1 <= Node.val <= 1000. Singly linked-list.
+// Reorder the linked list like so: Node1, NodeN-1, Node2, NodeN-2...
+// - Modify in-place. Do not return anything.
