a9bc03418958859b3050274b3eaec35754aee24d
Robin H <robin.hrzg@gmail.com>
2025-11-10 12:01:42 -0600

    Remove Nth node from end of list. (Linked list)

---
+++ 11102025.ts
@@ -0,0 +1,33 @@
+// Remove Nth node from end of list. (Linked list)
+
+function removeNthFromEnd(head: ListNode | null, n: number): ListNode | null {
+  const dummy = new ListNode(null, head);
+  let fast = dummy;
+  let slow = dummy;
+
+  // Step 1: Increment fast pointer n times.
+  for (let i = 0; i <= n; i++) {
+    fast = fast.next;
+  }
+
+  // Step 2: Increment both pointers until fast reaches end of list.
+  while (fast) {
+    fast = fast.next;
+    slow = slow.next;
+  }
+
+  // Step 3: Remove slow node.
+  slow.next = slow.next.next;
+
+  return dummy.next;
+}
+
+// console.log(removeNthFromEnd([1, 2, 3, 4, 5], 2), [1, 2, 3, 5]); // Remove the fourth node, as it's 2nd from the end.
+// console.log(removeNthFromEnd([1], 1), []);
+// console.log(removeNthFromEnd([1, 2], 1), [1]);
+// https://leetcode.com/problems/remove-nth-node-from-end-of-list/
+
+// head: ListNode | null. 1 <= number of nodes <= 30. 0 <= Node.val <= 100.
+// n: number. 1 <= n <= number of nodes.
+// Return the head after removing the nth node from the end of the list.
+// - Achieve this in one pass.
