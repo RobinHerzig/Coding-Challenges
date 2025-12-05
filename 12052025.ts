9f85691a9d5273a67a9b7ee65938bea5db415ad0
Robin H <robin.hrzg@gmail.com>
2025-12-05 13:01:34 -0600

    Car fleets. (Monotonic stack)

---
+++ 12052025.ts
@@ -0,0 +1,35 @@
+// Car fleets. (Monotonic stack)
+
+function carFleet(target: number, position: number[], speed: number[]): number {
+  const stack: number[] = [];
+
+  // Step 1: Combine position and speed and sort.
+  const vehicles = position.map((elem, index) => [elem, speed[index]]);
+  vehicles.sort((a, b) => a[0] - b[0]);
+
+  // Step 2: Monotonic stack to group into fleets.
+  for (const [position, speed] of vehicles) {
+    const time = (target - position) / speed;
+
+    while (time >= stack.at(-1)!) {
+      stack.pop();
+    }
+
+    stack.push(time);
+  }
+
+  return stack.length;
+}
+
+console.log(carFleet(12, [10, 8, 0, 5, 3], [2, 4, 1, 1, 3]), 3);
+console.log(carFleet(10, [3], [3]), 1);
+console.log(carFleet(100, [0, 2, 4], [4, 2, 1]), 1);
+// https://leetcode.com/problems/car-fleet/
+
+// target: number. Ending mile. 0 < target <= 10^5.
+// position: number[]. 1 <= position.length <= 10^5. 0 <= position[i] <= target. Starting mile of car.
+// speed: number[]. 1 <= position.length <= 10^5. 0 < position[i] <= 10^6. Speed of car.
+
+// Return the number of fleets that arrive at target.
+// - Cars cannot pass. Speed of car fleet is the minimum of all cars in the fleet.
+// - Cars are in the fleet if they catch up before or at target.
